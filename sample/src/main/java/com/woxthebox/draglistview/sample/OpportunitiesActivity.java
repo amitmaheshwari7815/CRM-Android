e8984646114a549acd33e169e902ed58749e0ffd
amitmaheshwari7815 <amitrander2017@gmail.com>
2018-04-03 12:00:53 +0530

    add some modification

--- sample/src/main/java/com/woxthebox/draglistview/sample/OpportunitiesActivity.java
+++ sample/src/main/java/com/woxthebox/draglistview/sample/OpportunitiesActivity.java
@@ -20,13 +20,33 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-public class OpportunitiesActivity extends AppCompatActivity{
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import cz.msebera.android.httpclient.Header;
+
+public class OpportunitiesActivity extends AppCompatActivity {
+    public static ArrayList opportunities;
+    public AsyncHttpClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_opportunities);
+
+        client = new AsyncHttpClient();
+        opportunities = new ArrayList();
+        getData();
 
 
         if (savedInstanceState == null) {
@@ -41,7 +61,7 @@ public class OpportunitiesActivity extends AppCompatActivity{
         transaction.replace(R.id.container, fragment, "fragment").commit();
     }
 
-//    @Override
+    //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
 //        getMenuInflater().inflate(R.menu.menu_main, menu);
 //        return true;
@@ -69,4 +89,56 @@ public class OpportunitiesActivity extends AppCompatActivity{
 //
 //        return super.onOptionsItemSelected(item);
 //    }
+    protected void getData() {
+        String serverURL = "http://10.0.2.2:8000/api/clientRelationships/relationships/?format=json";
+        client.get(serverURL, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, final JSONArray response) {
+                for (int i = 0; i < response.length(); i++) {
+                    JSONObject Obj = null;
+                    try {
+                        Obj = response.getJSONObject(i);
+
+                        JSONArray contacts = Obj.getJSONArray("contacts");
+                        Log.e("contactslength",""+contacts.length());
+                        for (int j=0; j<contacts.length();j++) {
+                            JSONObject jsonObject = contacts.getJSONObject(j);
+                            String Cname = jsonObject.getString("name");
+                            String company = jsonObject.getString("company");
+                            String email = jsonObject.getString("email");
+                            String cmobile = jsonObject.getString("mobile");
+                            String designation = jsonObject.getString("designation");
+                            boolean male = jsonObject.getBoolean("male");
+                        }
+                        HashMap hashMap = new HashMap();
+
+//                        hashMap.put("name", name);
+//                        hashMap.put("logo", logo);
+//                        hashMap.put("mobile", mobile);
+//                        hashMap.put("web", web);
+////                        pk.put(address, "address");
+//                        hashMap.put("street", street);
+//                        hashMap.put("name",Cname);
+//                        hashMap.put("company",company);
+//                        hashMap.put("email",email);
+//                        hashMap.put("mobile",cmobile);
+//                        hashMap.put("contacts",contacts);
+//                        hashMap.put("designation",designation);
+//                        hashMap.put("male",male);
+
+
+//                        pk.put(city, "city");
+//                        pk.put(state, "state");
+//                        pk.put(pincode, "pincode");
+//                        pk.put(country, "country");
+                        opportunities.add(hashMap);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        });
+    }
 }
